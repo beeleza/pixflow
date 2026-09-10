c3b2693b46a819153f42196a1885e5679c58b65d
beeleza <higorbeleza.dev@gmail.com>
2026-09-10 00:33:29 -0300

    refactor: remover import não utilizado de JsonDeserializer

--- src/main/java/com/beeleza/pixflow/adapter/out/kafka/KafkaConfig.java
+++ src/main/java/com/beeleza/pixflow/adapter/out/kafka/KafkaConfig.java
@@ -11,7 +11,6 @@ import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JacksonJsonDeserializer;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 @Configuration
 public class KafkaConfig {
